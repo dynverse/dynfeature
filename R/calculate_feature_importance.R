76225290f304784eb4564c2031ee617a9b4c098d
Wouter Saelens <wouter.saelens@gmail.com>
2018-04-26 10:52:42 +0200

    add mtry argument

--- R/calculate_feature_importance.R
+++ R/calculate_feature_importance.R
@@ -87,9 +87,14 @@ get_importance <- function(data, expression, method, method_params) {
     requireNamespace("ranger")
     # process ranger params
 
-    default_params <- list(dependent.variable.name="PREDICT",data = data, importance="impurity")
+    default_params <- list(
+      dependent.variable.name="PREDICT",
+      data = data,
+      importance="impurity",
+      mtry = function(x) ncol(x) * .01
+    )
     method_params <- list_modify(default_params, !!!method_params)
-    if ("mtry" %in% names(method_params)) {method_params$mtry <- method_params$mtry(expression)}
+    if (is.function(method_params$mtry)) {method_params$mtry <- method_params$mtry(expression)}
 
     # call ranger and get variable importance
     do.call(ranger::ranger, method_params)$variable.importance
