606d35f4cd481fbf0e1ffdefb4b3475a738e14f6
Wouter Saelens <wouter.saelens@gmail.com>
2019-03-18 12:00:56 +0100

    if sparse convert to matrix

--- R/calculate_feature_importances.R
+++ R/calculate_feature_importances.R
@@ -7,6 +7,9 @@
 calculate_feature_importances <- function(X, Y, fi_method = fi_ranger_rf_lite(), verbose = FALSE) {
   # if Y is a vector or a matrix, turn it into a data frame
   if (!is.data.frame(Y)) {
+    if ("dgCMatrix" %in% class(Y)) {
+      Y <- as.matrix(Y)
+    }
     Y <- as_data_frame(Y)
   }
 
